3b528245bf3c98b66c4fb2936ee3de9d8b92615b
isra el <vernu1997@gmail.com>
2022-03-24 07:30:05 +0300

    register device done

--- src/gateway/gateway.controller.ts
+++ src/gateway/gateway.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Post } from '@nestjs/common'
+import { GatewayService } from './gateway.service'
 
 @Controller('gateway')
-export class GatewayController {}
+export class GatewayController {
+  constructor(private readonly gatewayService: GatewayService) {}
+
+  @Post('/registerDevice')
+  async registerDevice(@Body() input: any) {
+    const data = await this.gatewayService.registerDevice(input)
+    return { data }
+  }
+}
--- src/gateway/gateway.service.ts
+++ src/gateway/gateway.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common'
-
+import { InjectModel } from '@nestjs/mongoose'
+import { Device, DeviceDocument } from './schemas/device.schema'
+import { Model } from 'mongoose'
 @Injectable()
-export class GatewayService {}
+export class GatewayService {
+  constructor(
+    @InjectModel(Device.name) private deviceModel: Model<DeviceDocument>,
+  ) {}
+
+  async registerDevice(input: any): Promise<any> {
+    const { fcmToken } = input
+    const newDevice = await this.deviceModel.create({ fcmToken })
+    return newDevice
+  }
+}
