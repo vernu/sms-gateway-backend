b38075a7143f6e7b2c3e1b08da69778d9d0e8ac1
isra el <vernu1997@gmail.com>
2022-03-24 08:10:33 +0300

    firebase-admin config

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import { VersioningType } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import * as firebase from 'firebase-admin'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -11,6 +12,23 @@ async function bootstrap() {
   app.enableVersioning({
     defaultVersion: '1',
     type: VersioningType.URI,
+  })
+
+  const firebaseConfig = {
+    type: 'service_account',
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    clientId: process.env.FIREBASE_CLIENT_ID,
+    authUri: 'https://accounts.google.com/o/oauth2/auth',
+    tokenUri: 'https://oauth2.googleapis.com/token',
+    authProviderX509CertUrl: 'https://www.googleapis.com/oauth2/v1/certs',
+    clientC509CertUrl: process.env.FIREBASE_CLIENT_C509_CERT_URL,
+  }
+
+  firebase.initializeApp({
+    credential: firebase.credential.cert(firebaseConfig),
   })
 
   app.enableCors()
