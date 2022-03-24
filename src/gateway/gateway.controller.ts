9d0e6e50eafbdb02ac76adedcd68fd17b0b23421
isra el <vernu1997@gmail.com>
2022-03-24 07:33:28 +0300

    update register device url

--- src/gateway/gateway.controller.ts
+++ src/gateway/gateway.controller.ts
@@ -5,7 +5,7 @@ import { GatewayService } from './gateway.service'
 export class GatewayController {
   constructor(private readonly gatewayService: GatewayService) {}
 
-  @Post('/registerDevice')
+  @Post('/devices')
   async registerDevice(@Body() input: any) {
     const data = await this.gatewayService.registerDevice(input)
     return { data }
