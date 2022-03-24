62b4b8ddd63fe66103ce97b6722bc2f448e0cd7a
isra el <vernu1997@gmail.com>
2022-03-24 21:50:46 +0300

    sendSMS endpoint done

--- src/gateway/gateway.controller.ts
+++ src/gateway/gateway.controller.ts
@@ -15,4 +15,10 @@ export class GatewayController {
     const data = await this.gatewayService.updateFCMToken(deviceId, input)
     return { data }
   }
+
+  @Post('/devices/:id/sendSMS')
+  async sendSMS(@Param('id') deviceId: string, @Body() smsData: any) {
+    const data = await this.gatewayService.sendSMS(deviceId, smsData)
+    return { data }
+  }
 }
--- src/gateway/gateway.service.ts
+++ src/gateway/gateway.service.ts
@@ -2,6 +2,7 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { Device, DeviceDocument } from './schemas/device.schema'
 import { Model } from 'mongoose'
+import * as firebaseAdmin from 'firebase-admin'
 @Injectable()
 export class GatewayService {
   constructor(
@@ -28,6 +29,45 @@ export class GatewayService {
     } else {
       device.fcmToken = fcmToken
       return await device.save()
+    }
+  }
+
+  async sendSMS(deviceId: string, smsData): Promise<any> {
+    const device = await this.deviceModel.findById(deviceId)
+
+    if (!device) {
+      throw new HttpException(
+        {
+          error: 'Device not found',
+        },
+        HttpStatus.NOT_FOUND,
+      )
+    } else {
+      const payload: any = {
+        // notification: {
+        //   title: 'SMS',
+        //   body: 'message',
+        // },
+        data: {
+          smsData: JSON.stringify(smsData),
+        },
+      }
+      try {
+        const response = await firebaseAdmin
+          .messaging()
+          .sendToDevice(device.fcmToken, payload, { priority: 'high' })
+
+        console.log('Successfully sent message:', response)
+        return response
+      } catch (e) {
+        console.log('Error sending message:', e)
+        throw new HttpException(
+          {
+            error: 'Failed to send SMS',
+          },
+          HttpStatus.BAD_REQUEST,
+        )
+      }
     }
   }
 }
